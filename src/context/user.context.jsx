ce44bcaeb252b5816d5c44f0d8a994ea53f68fce
aysswary <90493364+aysswary@users.noreply.github.com>
2025-05-29 13:20:45 +0530

    first commit

---
+++ src/context/user.context.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+    // update user data
+    const updateUser = (userData) => {
+        setUser(userData);
+    }
+
+    // to clear user data
+    const clearUser = () => {
+        setUser(null);
+    };
+
+    return (
+        <UserContext.Provider value={{ user, updateUser, clearUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export default UserProvider;
